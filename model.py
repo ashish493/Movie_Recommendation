10b81330ff0cc22a3b3e6b6367767dd027b8299d
ashish493 <malikashish493@gmail.com>
2020-04-22 19:41:28 +0530

    base of model

---
+++ model.py
@@ -0,0 +1,35 @@
+import numpy as np
+import pandas as pd
+import torch
+import torch.nn as nn
+import torch.nn.parallel
+import torch.optim as optim
+import torch.utils.data
+from torch.autograd import Variable
+
+movies = pd.read_csv('ml-1m/movies.dat', sep = '::', header = None, engine = 'python', encoding = 'utf-8')
+users = pd.read_csv('ml-1m/users.dat', sep = '::', header = None, engine = 'python', encoding = 'utf-8')
+ratings = pd.read_csv('ml-1m/ratings.dat', sep = '::', header = None, engine = 'python', encoding = 'utf-8')
+
+training_set = pd.read_csv('ml-100k/u1.base', delimiter = '\t')
+training_set = np.array(training_set, dtype = 'int')
+test_set = pd.read_csv('ml-100k/u1.test', delimiter = '\t')
+test_set = np.array(test_set, dtype = 'int')
+
+num_users = int(max(max(training_set[:,0]), max(test_set[:,0])))
+num_movies = int(max(max(training_set[:,1]), max(test_set[:,1])))
+
+def convert(data):
+    new_data = []
+    for id_users in range(1, num_users + 1):
+        id_movies = data[:,1][data[:,0] == id_users]
+        id_ratings = data[:,2][data[:,0] == id_users]
+        ratings = np.zeros(num_movies)
+        ratings[id_movies - 1] = id_ratings
+        new_data.append(list(ratings))
+    return new_data
+training_set = convert(training_set)
+test_set = convert(test_set)
+
+
+
